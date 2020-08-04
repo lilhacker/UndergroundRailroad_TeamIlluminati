7694309983a8009f47a4fa17c48e205cf8087da6
lilhacker <gaurav.palewar@gmail.com>
2020-08-04 10:32:08 +0530

    Add files via upload

--- secret.js
+++ secret.js
@@ -23,3 +23,23 @@ function image() {
 		window.open('UGRR.jpg', '_blank');
 	}
 }
+
+
+// Making the Map
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var Alabama = {lat: 32.3182, lng: -86.9023};
+  var neIndiana = {lat: 38.6175, lng: -86.3580};
+  var india = {}
+
+
+  // The map, centered at Alabama
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: Alabama, mapTypeId: google.maps.MapTypeId.HYBRID});
+  // The marker, positioned at Alabama
+  var markerAlabama = new google.maps.Marker({position: Alabama, map: map});
+  var markerneIndiana = new google.maps.Marker({position: neIndiana, map: map});
+
+}
